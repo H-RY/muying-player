6b9e3facc93542302b61fcde9f75a2b0f3c21392
Administrator <849541922@qq.com>
2022-02-28 14:59:34 +0800

    测试git

--- main.cpp
+++ main.cpp
@@ -2,8 +2,15 @@
 // Created by Administrator on 2022/2/27.
 //
 #include "iostream"
+#include "vector"
 using namespace std;
 int main(){
-    cout<<"hello world"<<endl;
+    vector<int> temp = {1,2,3,4};
+    temp.push_back(5);
+    for(int i = 0; i< temp.size(); i++){
+        cout<<temp[i]<<endl;
+    }
+    cout<<temp.size()<<endl;
+    cout<<temp.capacity()<<endl;
     return 0;
 }
